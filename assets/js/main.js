604de878e0971287128fc5fc55e537f6629040a3
Jeremy <ravenrevolve@gmail.com>
2018-02-27 20:50:39 -0600

    final push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 //Character Stats
 var gameStart=false
 var playerDamage=0
@@ -10,11 +11,12 @@ var mageTrue=false
 var adcPlayer=false
 var tankPlayer=false
 var magePlayer=false
-var support=false
+var supportPlayer=false
 var tankAlive=true
 var adcAlive=true
 var mageAlive=true
 var supportAlive=true
+var playerAlive=true
 var adc = {
     health:100,
     strength:100
@@ -105,7 +107,6 @@ $("#mage").on("click", function(){
     //Enemy select
     $("#eniTank").on("click", function(){
         $("#eniTankBox").appendTo(".arena").attr("id", "trueEnemy")
-        $("#eniTankBox").css("background-color", "black")
         tankTrue=true
     })
     $("#eniAdc").on("click", function(){
@@ -142,189 +143,173 @@ $("#support").on("click", function(){
         mageTrue=true
     })
 });
-//AttackBTN
+console.log("gamestart"+gameStart,"playerdamage"+playerDamage,"cpudamage"+cpuDamage,"tanktrue"+tankTrue,"adctrue"+adcTrue,"supporttrue"+supportTrue,"magetrue"+mageTrue,
+"adcplayer"+adcPlayer,"tankplayer"+tankPlayer,"mageplayer"+magePlayer,"supportplayer"+supportPlayer,"tankalive"+tankAlive,"adcalive"+adcAlive,
+"magealive"+mageAlive,"suportAlive"+supportAlive,"playerAlive"+playerAlive)
+//attack button(bug)
 $("#attackBtn").on("click", function(){
-    if (magePlayer=true, tankTrue=true){
-   var playerDamage = Math.floor((Math.random() * mage.strength))
-   var cpuDamage= Math.floor((Math.random() * tank.strength))
-    tank.health=tank.health-playerDamage
-   mage.health=mage.health-cpuDamage
-   console.log(playerDamage, cpuDamage, magePlayer, tankTrue, )
-    document.getElementById("tankHealth").innerHTML = tank.health;
-    document.getElementById("mageHealth").innerHTML = mage.health;
+    console.log("gamestart"+gameStart,"playerdamage"+playerDamage,"cpudamage"+cpuDamage,"tanktrue"+tankTrue,"adctrue"+adcTrue,"supporttrue"+supportTrue,"magetrue"+mageTrue,
+    "adcplayer"+adcPlayer,"tankplayer"+tankPlayer,"mageplayer"+magePlayer,"supportplayer"+supportPlayer,"tankalive"+tankAlive,"adcalive"+adcAlive,
+    "magealive"+mageAlive,"suportAlive"+supportAlive,"playerAlive"+playerAlive)
+    if ((magePlayer=true)&& (tankTrue=true)){
+        var playerDamage = Math.floor((Math.random() * mage.strength))
+        var cpuDamage= Math.floor((Math.random() * tank.strength))
+        tank.health=tank.health-playerDamage
+        mage.health=mage.health-cpuDamage
+        document.getElementById("tankHealth").innerHTML = tank.health;
+        document.getElementById("mageHealth").innerHTML = mage.health;
     }
-    if (tank.health<=0){
-        $("eniTank", "eniTankBox").css("display", "none")
-        tankTrue=false
-        tankAlive=false
+    else  if ((magePlayer=true) && (adcTrue=true)){
+        var playerDamage = Math.floor((Math.random() * mage.strength))
+        var cpuDamage= Math.floor((Math.random() * adc.strength))
+        adc.health=adc.health-playerDamage
+        mage.health=mage.health-cpuDamage
+        document.getElementById("adcHealth").innerHTML = adc.health;
+        document.getElementById("mageHealth").innerHTML = mage.health;
+        }
+    else if((magePlayer=true) && (supportTrue=true)){
+        var playerDamage = Math.floor((Math.random() * mage.strength))
+        var cpuDamage= Math.floor((Math.random() * support.strength))
+        support.health=support.health-playerDamage
+        mage.health=mage.health-cpuDamage
+        console.log(playerDamage, cpuDamage, magePlayer, supportTrue, )
+        document.getElementById("supportHealth").innerHTML = support.health;
+        document.getElementById("mageHealth").innerHTML = mage.health;
+        }
+    if ((tankPlayer=true) && (adcTrue=true)){
+        var playerDamage = Math.floor((Math.random() * tank.strength))
+        var cpuDamage= Math.floor((Math.random() * adc.strength))
+        adc.health=adc.health-playerDamage
+        tank.health=tank.health-cpuDamage
+        document.getElementById("adcHealth").innerHTML = adc.health;
+        document.getElementById("tankHealth").innerHTML = tank.health;
+        }
+    else if ((tankPlayer=true) && (mageTrue=true)){
+        var playerDamage = Math.floor((Math.random() * tank.strength))
+        var cpuDamage= Math.floor((Math.random() * mage.strength))
+        mage.health=mage.health-playerDamage
+        tank.health=tank.health-cpuDamage
+        document.getElementById("mageHealth").innerHTML = mage.health;
+        document.getElementById("tankHealth").innerHTML = tank.health;
+        }
+    else if ((tankPlayer=true) && (supportTrue=true)){
+        var playerDamage = Math.floor((Math.random() * tank.strength))
+        var cpuDamage= Math.floor((Math.random() * support.strength))
+        support.health=support.health-playerDamage
+        tank.health=tank.health-cpuDamage
+        document.getElementById("supportHealth").innerHTML = support.health;
+        document.getElementById("tankHealth").innerHTML = tank.health;
+        }
+    if ((adcPlayer=true) && (tankTrue=true)){
+        var playerDamage = Math.floor((Math.random() * adc.strength))
+        var cpuDamage= Math.floor((Math.random() * tank.strength))
+        tank.health=tank.health-playerDamage
+        adc.health=adc.health-cpuDamage
+        document.getElementById("tankHealth").innerHTML = tank.health;
+        document.getElementById("adcHealth").innerHTML = adc.health;
+        }
+    else if ((adcPlayer=true) && (mageTrue=true)){
+        var playerDamage = Math.floor((Math.random() * adc.strength))
+        var cpuDamage= Math.floor((Math.random() * mage.strength))
+        mage.health=mage.health-playerDamage
+        adc.health=adc.health-cpuDamage
+        document.getElementById("mageHealth").innerHTML = mage.health;
+        document.getElementById("adcHealth").innerHTML = adc.health;
+        }
+    else if ((adcPlayer=true) && (supportTrue=true)){
+        var playerDamage = Math.floor((Math.random() * adc.strength))
+        var cpuDamage= Math.floor((Math.random() * support.strength))
+        support.health=support.health-playerDamage
+        adc.health=adc.health-cpuDamage
+        document.getElementById("supportHealth").innerHTML = support.health;
+        document.getElementById("adcHealth").innerHTML = adc.health;
+        }
+    if ((supportPlayer=true) && (tankTrue=true)){
+        var playerDamage = Math.floor((Math.random() * support.strength))
+        var cpuDamage= Math.floor((Math.random() * tank.strength))
+        tank.health=tank.health-playerDamage
+        support.health=support.health-cpuDamage
+        document.getElementById("tankHealth").innerHTML = tank.health;
+        document.getElementById("supportHealth").innerHTML = support.health;
+        }
+    else if ((supportPlayer=true) && (mageTrue=true)){
+        var playerDamage = Math.floor((Math.random() * support.strength))
+        var cpuDamage= Math.floor((Math.random() * mage.strength))
+        mage.health=mage.health-playerDamage
+        support.health=support.health-cpuDamage
+        document.getElementById("mageHealth").innerHTML = mage.health;
+        document.getElementById("supportHealth").innerHTML = support.health;
+        }   
+    else if ((supportPlayer=true) && (adcTrue=true)){
+        var playerDamage = Math.floor((Math.random() * support.strength))
+        var cpuDamage= Math.floor((Math.random() * adc.strength))
+        adc.health=adc.health-playerDamage
+        support.health=support.health-cpuDamage
+        document.getElementById("adcHealth").innerHTML = adc.health;
+        document.getElementById("supportHealth").innerHTML = support.health;
+        }
+    console.log("tank"+tankTrue, "adc"+adcTrue, "support"+supportTrue, "mage"+mageTrue)
+if (adc.health<=0){
+    $("eniAdc", "eniADCBox").css("display", "none")
+    adcTrue=false
+    adcAlive=false
+    console.log(adcAlive)
+}
+ if (tank.health<=0){
+    $("eniTank", "eniTankBox").css("display", "none")
+    tankTrue=false
+    tankAlive=false
+}   
+if (tank.health<=0){
+    $("eniSupport", "eniSupportBox").css("display", "none")
+    supportTrue=false
+    supportAlive=false
+}
+if (tank.health<=0){
+     $("eniAdc", "eniAdcBox").css("display", "none")
+    adcTrue=false
+    adcAlive=false 
+}
+if (mage.health<=0){
+    $("enimage", "eniMageBox").css("display", "none")
+    mageTrue=false
+    mageAlive=false
+}
+if (support.health<=0){
+    $("eniSupport", "eniSupportBox").css("display", "none")
+    supportTrue=false
+    supportAlive=false
+}
+if (tank.health<=0){
+    $("eniTank", "eniTankBox").css("display", "none")
+    tankTrue=false
+    tankAlive=false
+}
+if (mage.health<=0){
+    $("eniMage", "eniMageBox").css("display", "none")
+    mageTrue=false
+    mageAlive=false
     }
-});
-$("#attackBtn").on("click", function(){
-    if (magePlayer=true, adcTrue=true)
-   var playerDamage = Math.floor((Math.random() * mage.strength))
-   var cpuDamage= Math.floor((Math.random() * adc.strength))
-    adc.health=adc.health-playerDamage
-   mage.health=mage.health-cpuDamage
-    document.getElementById("adcHealth").innerHTML = adc.health;
-    document.getElementById("mageHealth").innerHTML = mage.health;
-    if (adc.health<=0){
-        $("eniAdc", "eniADCBox").css("display", "none")
-        adcTrue=false
-        adcAlive=false
-        console.log(adcAlive)
-    }
-});
-
-$("#attackBtn").on("click", function(){
-    if (magePlayer=true, supportTrue=true){
-   var playerDamage = Math.floor((Math.random() * mage.strength))
-   var cpuDamage= Math.floor((Math.random() * support.strength))
-    support.health=support.health-playerDamage
-   mage.health=mage.health-cpuDamage
-   console.log(playerDamage, cpuDamage, magePlayer, tankTrue, )
-    document.getElementById("supportHealth").innerHTML = support.health;
-    document.getElementById("mageHealth").innerHTML = mage.health;
-    }
-    if (tank.health<=0){
-        $("eniSupport", "eniSupportBox").css("display", "none")
-        supportTrue=false
-        supportAlive=false
-    } 
-});
-$("#attackBtn").on("click", function(){
-    if (tankPlayer=true, adcTrue=true){
-   var playerDamage = Math.floor((Math.random() * tank.strength))
-   var cpuDamage= Math.floor((Math.random() * adc.strength))
-    adc.health=adc.health-playerDamage
-   tank.health=tank.health-cpuDamage
-    document.getElementById("adcHealth").innerHTML = adc.health;
-    document.getElementById("tankHealth").innerHTML = tank.health;
-    }
-    if (tank.health<=0){
-        $("eniAdc", "eniAdcBox").css("display", "none")
-        adcTrue=false
-        adcAlive=false 
-    }
-});
-$("#attackBtn").on("click", function(){
-    if (tankPlayer=true, mageTrue=true){
-   var playerDamage = Math.floor((Math.random() * tank.strength))
-   var cpuDamage= Math.floor((Math.random() * mage.strength))
-    mage.health=mage.health-playerDamage
-   tank.health=tank.health-cpuDamage
-    document.getElementById("mageHealth").innerHTML = mage.health;
-    document.getElementById("tankHealth").innerHTML = tank.health;
-    }
-    if (mage.health<=0){
-        $("enimage", "eniMageBox").css("display", "none")
-        mageTrue=false
-        mageAlive=false
-    }
-});
-$("#attackBtn").on("click", function(){
-    if (tankPlayer=true, supportTrue=true){
-   var playerDamage = Math.floor((Math.random() * tank.strength))
-   var cpuDamage= Math.floor((Math.random() * support.strength))
-    support.health=support.health-playerDamage
-   tank.health=tank.health-cpuDamage
-    document.getElementById("supportHealth").innerHTML = support.health;
-    document.getElementById("tankHealth").innerHTML = tank.health;
-    }
-    if (support.health<=0){
-        $("eniSupport", "eniSupportBox").css("display", "none")
-        supportTrue=false
-        supportAlive=false
-    } 
-});
-$("#attackBtn").on("click", function(){
-    if (adcPlayer=true, tankTrue=true){
-   var playerDamage = Math.floor((Math.random() * adc.strength))
-   var cpuDamage= Math.floor((Math.random() * tank.strength))
-    tank.health=tank.health-playerDamage
-   adc.health=adc.health-cpuDamage
-    document.getElementById("tankHealth").innerHTML = tank.health;
-    document.getElementById("adcHealth").innerHTML = adc.health;
-    }
-    if (tank.health<=0){
-        $("eniTank", "eniTankBox").css("display", "none")
-        tankTrue=false
-        tankAlive=false
-    }
-});
-$("#attackBtn").on("click", function(){
-    if (adcPlayer=true, mageTrue=true){
-   var playerDamage = Math.floor((Math.random() * adc.strength))
-   var cpuDamage= Math.floor((Math.random() * mage.strength))
-    mage.health=mage.health-playerDamage
-   adc.health=adc.health-cpuDamage
-    document.getElementById("mageHealth").innerHTML = mage.health;
-    document.getElementById("adcHealth").innerHTML = adc.health;
-    }
-    if (mage.health<=0){
-        $("eniMage", "eniMageBox").css("display", "none")
-        mageTrue=false
-       mageAlive=false
-    }
-});
-$("#attackBtn").on("click", function(){
-    if (adcPlayer=true, supportTrue=true){
-   var playerDamage = Math.floor((Math.random() * adc.strength))
-   var cpuDamage= Math.floor((Math.random() * support.strength))
-    support.health=support.health-playerDamage
-   adc.health=adc.health-cpuDamage
-    document.getElementById("supportHealth").innerHTML = support.health;
-    document.getElementById("adcHealth").innerHTML = adc.health;
-    }
-    if (support.health<=0){
-        $("eniSupport", "eniSupportBox").css("display", "none")
-        supportTrue=false
-        supportAlive=false 
-    }
-});
-$("#attackBtn").on("click", function(){
-    if (supportPlayer=true, tankTrue=true){
-   var playerDamage = Math.floor((Math.random() * support.strength))
-   var cpuDamage= Math.floor((Math.random() * tank.strength))
-    tank.health=tank.health-playerDamage
-   support.health=support.health-cpuDamage
-    document.getElementById("tankHealth").innerHTML = tank.health;
-    document.getElementById("supportHealth").innerHTML = support.health;
-    }
-    if (tank.health<=0){
-        $("eniTank", "eniTankBox").css("display", "none")
-        tankTrue=false
-        tankAlive=false 
-    }     
-});
-$("#attackBtn").on("click", function(){
-    if (supportPlayer=true, mageTrue=true){
-   var playerDamage = Math.floor((Math.random() * support.strength))
-   var cpuDamage= Math.floor((Math.random() * mage.strength))
-    mage.health=mage.health-playerDamage
-   support.health=support.health-cpuDamage
-    document.getElementById("mageHealth").innerHTML = mage.health;
-    document.getElementById("supportHealth").innerHTML = support.health;
-    }
-    if (mage.health<=0){
-        $("eniMage", "eniMageBox").css("display", "none")
-        mageTrue=false
-        mageAlive=false
-    }    
-});
-$("#attackBtn").on("click", function(){
-    if (supportPlayer=true, adcTrue=true){
-   var playerDamage = Math.floor((Math.random() * support.strength))
-   var cpuDamage= Math.floor((Math.random() * adc.strength))
-    adc.health=adc.health-playerDamage
-   support.health=support.health-cpuDamage
-    document.getElementById("adcHealth").innerHTML = adc.health;
-    document.getElementById("supportHealth").innerHTML = support.health;
-    }
-    if (adc.health<=0){
-        $("eniAdc", "eniAdcBox").css("display", "none")
-        adcTrue=false
-        adcAlive=false
-    }   
+if (support.health<=0){
+    $("eniSupport", "eniSupportBox").css("display", "none")
+    supportTrue=false
+    supportAlive=false 
+}
+if (tank.health<=0){
+    $("eniTank", "eniTankBox").css("display", "none")
+    tankTrue=false
+    tankAlive=false 
+}
+if (mage.health<=0){
+    $("eniMage", "eniMageBox").css("display", "none")
+    mageTrue=false
+    mageAlive=false
+} 
+if (adc.health<=0){
+    $("eniAdc", "eniAdcBox").css("display", "none")
+    adcTrue=false
+    adcAlive=false
+} 
 });
 //lose conditions 
 //win conditions
@@ -341,4 +326,5 @@ if (tankAlive=false, supportAlive=false, adcAlive=false){
 
     alert("YouWin!!!!!!")
 }
-});
+
+})
